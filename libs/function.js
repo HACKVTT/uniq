9c9e5939100e58017a531c60435bf2d8fca861a8
HACKVTT <HACKVTT@gmail.com>
2015-10-26 22:36:39 +0700

    update book number day

--- libs/function.js
+++ libs/function.js
@@ -85,51 +85,6 @@ $(document).ready(function(){
             });
         }
     });
-    
-    // var m_xcl_sum_view = $('.xcl_sum_view');
-    // var m_xcl_itm = $('.xcl-itm');
-    // var m_sum = 0;
-    // m_xcl_itm.each(function(i, d) {
-    //     m_sum += parseInt($(d).val());
-    // });
-//////////////////////////////////////////////////////////
-    // m_xcl_sum.val(m_sum);
-    // m_xcl_sum_view.text(m_sum);
-
-    // $('.plus').each(function(i, m) {
-    //     $(m).click(function(e) {
-    //         var m_input = $(this).prev();
-    //         var m_input_val = parseInt(m_input.val()) + 1;
-    //         m_sum = 0;
-    //         if (m_input_val >= 0) {
-    //             m_input.val(m_input_val);
-    //             m_xcl_itm.each(function(i, d) {
-    //                 m_sum += parseInt($(d).val());
-    //             });
-    //             m_xcl_sum.val(m_sum);
-    //             m_xcl_sum_view.text(m_sum);
-    //         } else {
-    //             m_input_val = 0;
-    //         }
-    //     });
-    // });
-    // $('.minus').each(function(i, m) {
-    //     $(m).click(function(e) {
-    //         var m_input = $(this).next();
-    //         var m_input_val = parseInt(m_input.val()) - 1;
-    //         m_sum = 0;
-    //         if (m_input_val >= 0) {
-    //             m_input.val(m_input_val);
-    //             m_xcl_itm.each(function(i, d) {
-    //                 m_sum += parseInt($(d).val());
-    //             });
-    //             m_xcl_sum.val(m_sum);
-    //             m_xcl_sum_view.text(m_sum);
-    //         } else {
-    //             m_input_val = 0;
-    //         }
-    //     });
-    // });
 
     // Auto Complete
 
@@ -164,6 +119,71 @@ $(document).ready(function(){
   });
 
 
+    //======date Picker============//
+    $( ".datepicker" ).datepicker();
 
-    $( ".datepicker" ).datepicker();	
-})
+    //=========Book Adroom=========//
+
+    var i = 1;
+
+
+    $(".js_add_room").on("click", function(e) {
+        e.preventDefault();
+        i++;
+
+        var htmlroom = '<div class="clearfix"></div><div class="quantity"><div class="col-xs-3">Room '+i+ '</div><div class="form-group"><label for="from">Adults<br></label><div class="select-div"><select name="" id=""><option value="1">1</option></select></div><div class="clearfix"></div></div><div class="form-group"><label for="to">Seniors<br></label><div class="select-div"><select name="" id=""><option value="1">1</option></select></div></div></div>'; 
+              
+        $(this).parent().siblings(".quantity").append(htmlroom);
+    });
+
+
+  $(".datepicker").datepicker().on("change", function() {
+     var number_night = getNumberDate();
+    $(".js_number_night").text(number_night);
+  });
+
+
+
+
+
+});
+
+
+    function getNumberDate() {
+        var checkin_time = $(".js_name_checkin").val();
+        var checkout_time = $(".js_name_checkout").val();
+            
+        var mydate_checkin = checkin_time.split('/'); 
+        var mydate_checkout = checkout_time.split('/');
+
+        var mydate_start = new Date();
+        mydate_start.setDate(mydate_checkin[1]);
+        mydate_start.setMonth(mydate_checkin[0]);
+        mydate_start.setFullYear(mydate_checkin[2]);
+        mydate_start.setHours(0);
+        mydate_start.setMinutes(0);
+        mydate_start.setSeconds(0);
+        mydate_start.setMilliseconds(0);
+
+        var mytime_start = mydate_start.getTime();
+        // console.log(mytime_start);
+        // console.log("Mydate: " + mydate_start);
+        var mydate_end = new Date();
+        mydate_end.setDate(mydate_checkout[1]);
+        mydate_end.setMonth(mydate_checkout[0]);
+        mydate_end.setFullYear(mydate_checkout[2]);
+        mydate_end.setHours(0);
+        mydate_end.setMinutes(0);
+        mydate_end.setSeconds(0);
+        mydate_end.setMilliseconds(0);   
+
+        var mytime_end = mydate_end.getTime();
+        var mytime = mytime_end - mytime_start;
+
+        var number_date = mytime/(1000*60*60*24);
+        if (number_date <0) {
+            number_date = 0;
+        }
+
+        return number_date;
+    }
